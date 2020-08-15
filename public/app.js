46c059e0dd3bd9fa8800d06359d7ef02055331c1
meda996 <petar.dulovic7@Gmail.com>
2020-08-16 00:04:46 +0200

    Novo

--- public/app.js
+++ public/app.js
@@ -1,13 +1,17 @@
-  
-if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker
-      .register('./service-worker.js', { scope: './' })
-      .then(function(registration) {
-        console.log("Service Worker Registered");
-      })
-      .catch(function(err) {
-        console.log("Service Worker Failed to Register", err);
-      })
-  
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+  // Registration was successful
+  console.log('Registered!');
+  }, function(err) {
+  // registration failed :(
+  console.log('ServiceWorker registration failed: ', err);
+  }).catch(function(err) {
+  console.log(err);
+  });
+  });
+  } else {
+  console.log('service worker is not supported');
   }
+// service-worker.js
