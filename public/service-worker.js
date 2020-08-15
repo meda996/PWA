c80a5ea8ee5a54abe84c45b4cce8148f4a98335a
meda996 <petar.dulovic7@Gmail.com>
2020-08-15 23:37:55 +0200

    PWA ZA MOBILNO2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,13 @@
-if('serviceWorker' in navigator){
+  
+if ('serviceWorker' in navigator) {
 
     navigator.serviceWorker
-    .register('./service-worker.js', { scope: './'})
-    .then(function(registration){
-        console.log("Service Worker Registered", registration)
-    })
-    .catch(function(err){
-        console.log("Service Worker failed to register", err )
-    })
-} 
+      .register('./service-worker.js', { scope: './' })
+      .then(function(registration) {
+        console.log("Service Worker Registered");
+      })
+      .catch(function(err) {
+        console.log("Service Worker Failed to Register", err);
+      })
+  
+  }
